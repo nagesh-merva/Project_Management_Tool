bde7cdbdec3b74a82a46e5eaa304755781acd05a
prathamnaik12 <148609885+prathamnaik12@users.noreply.github.com>
2025-07-10 23:19:07 +0530

    integrated addnew maintenance and report and update hosting details

--- frontend/src/components/Singleprojects/QuickActions.jsx
+++ frontend/src/components/Singleprojects/QuickActions.jsx
@@ -91,7 +91,7 @@ const QuickActions = ({ links }) => {
 
     return (
         <>
-            <div className="h-fit w-full grid gap-2 bg-gray-50 rounded-lg shadow-lg p-4">
+            <div className="h-fit w-full grid gap-2 bg-gray-50 rounded-lg shadow-lg p-4 ">
                 <div className="flex gap-2 items-center justify-between mb-2">
                     <div className="flex gap-2 items-center" >
                         <FastForward className="h-5 w-7" />
@@ -101,22 +101,23 @@ const QuickActions = ({ links }) => {
                         Update
                     </button>
                 </div>
-
-                {actions.map((action, idx) => (
-                    <button
-                        key={idx}
-                        onClick={action.onClick}
-                        className={`w-full h-[41px] flex items-center  gap-4 px-6 text-sm md:text-base lg:text-md font-semibold rounded-lg border-2 transition-all hover:scale-95 
+                <div className="h-fit w-full grid gap-2 bg-gray-50 rounded-xl max-h-80 overflow-y-auto custom-scrollbar pt-2">
+                    {actions.map((action, idx) => (
+                        <button
+                            key={idx}
+                            onClick={action.onClick}
+                            className={`w-full h-[41px] flex items-center  gap-4 px-6 text-sm md:text-base lg:text-md font-semibold rounded-lg border-2 transition-all hover:scale-95 
                         ${idx % 2 === 0
-                                ? "bg-btncol text-white hover:bg-btncol/60 border-purple-600"
-                                : "bg-white text-slate-700 hover:bg-gray-100 border-gray-400"
-                            }`}
-                    >
-                        <div className="w-1/5 h-full"></div>
-                        {action.icon}
-                        {action.label}
-                    </button>
-                ))}
+                                    ? "bg-btncol text-white hover:bg-btncol/60 border-purple-600"
+                                    : "bg-white text-slate-700 hover:bg-gray-100 border-gray-400"
+                                }`}
+                        >
+                            <div className="w-1/5 h-full"></div>
+                            {action.icon}
+                            {action.label}
+                        </button>
+                    ))}
+                </div>
             </div>
             <PopupForm
                 isVisible={showPopup}
